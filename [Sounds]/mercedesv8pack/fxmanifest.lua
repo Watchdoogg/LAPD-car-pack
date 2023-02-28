3163ae712ea6eaf312bce724c5f74c89d9563aa7
Watchdog <97258576+Watchdoogg@users.noreply.github.com>
2023-02-28 19:05:34 +0100

    Add files via upload

---
+++ [Sounds]/mercedesv8pack/fxmanifest.lua
@@ -0,0 +1,23 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+files {
+  'audioconfig/mercedesm113_game.dat151.rel',
+  'audioconfig/mercedesm113_sounds.dat54.rel',
+  'sfx/dlc_mercedesm113/mercedesm113.awc',
+  'sfx/dlc_mercedesm113/mercedesm113_npc.awc',
+
+  'audioconfig/mercedesm155_game.dat151.rel',
+  'audioconfig/mercedesm155_sounds.dat54.rel',
+  'sfx/dlc_mercedesm155/mercedesm155.awc',
+  'sfx/dlc_mercedesm155/mercedesm155_npc.awc'
+}
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/mercedesm113_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/mercedesm113_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_mercedesm113'
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/mercedesm155_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/mercedesm155_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_mercedesm155'
---
+++ [Sounds]/mercm177/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+files {
+  'audioconfig/mercm177_game.dat151.rel',
+  'audioconfig/mercm177_sounds.dat54.rel',
+  'sfx/dlc_mercm177/mercm177.awc',
+  'sfx/dlc_mercm177/mercm177_npc.awc'
+}
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/mercm177_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/mercm177_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_mercm177'
